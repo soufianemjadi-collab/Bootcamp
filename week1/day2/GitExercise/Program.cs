d8fb98686c60e30e99b1d1261007c41c30d16d7b
soufiane mjadi <soufianemjadi@gmail.com>
2025-11-27 15:49:08 +0100

    New Update

---
+++ week1/day2/GitExercise/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Game Started!");
+        Console.WriteLine("Loading...");
+        Console.WriteLine("I am Here...");
+    }
+}
